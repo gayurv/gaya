6533c6199d0d729da3ecc32aae4e2d66e257e1ce
gayurv <gayurv99@gamil.com>
2024-04-11 13:07:09 +0530

    Hai

---
+++ src/main/java/com/Baseclass/Library.java
@@ -0,0 +1,21 @@
+package com.Baseclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Library {
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+		
+	}
+
+}
